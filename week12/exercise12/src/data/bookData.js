a4ef89c11af035233e21f7d23265d094ffb35568
maribethromslo <mromslo@mcad.edu>
2026-04-17 01:18:53 -0500

    Add files via upload

---
+++ week12/exercise12/src/data/bookData.js
@@ -0,0 +1,22 @@
+const books = [
+  {
+    id: 1,
+    title: "The Creative Act",
+    author: "Rick Rubin",
+    description: "A reflection on creativity as a way of being."
+  },
+  {
+    id: 2,
+    title: "Big Magic",
+    author: "Elizabeth Gilbert",
+    description: "Thoughts on living a creative life beyond fear."
+  },
+  {
+    id: 3,
+    title: "Steal Like an Artist",
+    author: "Austin Kleon",
+    description: "A guide to embracing influence and making work."
+  }
+];
+
+export default books;
