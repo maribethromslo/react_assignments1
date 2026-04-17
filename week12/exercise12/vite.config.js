03d41e33c716bd5f93cd9a7ed4ce9428ab909d36
maribethromslo <mromslo@mcad.edu>
2026-04-17 02:10:51 -0500

    Fix base path in Vite configuration

--- week12/exercise12/vite.config.js
+++ week12/exercise12/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react_assignments1/week12/exercise12',
+  base: '/react_assignments1/week12/exercise12/',
 })
