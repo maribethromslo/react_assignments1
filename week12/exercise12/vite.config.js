1d80aa9e940c1ad5c56509c0583e90d03e864e1e
maribethromslo <mromslo@mcad.edu>
2026-04-17 01:01:52 -0500

    Delete week12/exercise12 directory

--- week12/exercise12/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/react_assignments/week11/exercise11/',
-})
