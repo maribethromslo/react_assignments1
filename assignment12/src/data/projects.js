3b386c246bef4ebfca6d0cb31a6a4965cc4fb09f
maribethromslo <mromslo@mcad.edu>
2026-04-20 01:01:17 -0500

    Add files via upload

---
+++ assignment12/src/data/projects.js
@@ -0,0 +1,54 @@
+const projects = [
+  { id: 1, name: "Rainbow Squares", 
+    description: "A grid of colorful squares arranged in a vibrant, geometric rainbow pattern.", 
+    url: "https://editor.p5js.org/mromslo/full/xGM0zK7xS", 
+    image: "/react_assignments1/week12/assignment12/img/rainbow-squares.gif"
+  },
+  { id: 2, name: "Albers Pulse",
+    description: "Squares of shifting colors inspired by Josef Albers' color theory experiments.",
+    url: "https://editor.p5js.org/mromslo/full/cwK4SoVAS",
+     image: "/react_assignments1/week12/assignment12/img/albers-pulse.gif"
+  },
+  { id: 3, name: "Apple Car", 
+    description: "A playful pixel-art style illustration of an apple-shaped car.", 
+    url: "https://editor.p5js.org/mromslo/full/GQmK1EiNY", 
+    image: "/react_assignments1/week12/assignment12/img/apple-car.png"
+  },
+  { id: 4, name: "Tickle For Birdsong",
+    description: " An interactive sketch where touching the canvas triggers playful birdsong sounds.",
+    url: "https://editor.p5js.org/mromslo/full/53F8dY63v",
+    image: "/react_assignments1/week12/assignment12/img/birds.gif"
+  },
+  { id: 5, name: "Rainbow Explosion",
+    description: "A burst of rainbow colors radiating outward from a central point in an explosive display.",
+    url: "https://editor.p5js.org/mromslo/full/qIcRQ8YIB",
+    image: "/react_assignments1/week12/assignment12/img/rainbow_explosion.gif"
+  },
+  { id: 6, name: "Rainbow Starburst",
+    description: "A starburst of rainbow-colored rays emanating from the center of the canvas.",
+    url: "https://editor.p5js.org/mromslo/full/dlaOZD5Pi",
+    image: "/react_assignments1/week12/assignment12/img/rainbow-starburst.png"
+  },
+  { id: 7, name: "Snow",
+    description: "Animated snowflakes falling.",
+    url: "https://editor.p5js.org/mromslo/full/RTZpCdax_",
+    image: "/react_assignments1/week12/assignment12/img/snow.gif"
+  },
+  { id: 8, name: "Snowflake",
+    description: "A single snowflake drawn with geometric symmetry.",
+    url: "https://editor.p5js.org/mromslo/full/dpTbgD_cp",
+    image: "/react_assignments1/week12/assignment12/img/snowflake.png"
+  },
+  { id: 9, name: "Sol LeWitt",
+    description: "A generative wall drawing inspired by the minimalist instruction-based art of Sol LeWitt.",
+    url: "https://editor.p5js.org/mromslo/full/c_lDWgN5u",
+    image: "/react_assignments1/week12/assignment12/img/sol-lewitt.gif"
+  },
+  { id: 10, name: "Star Grid",
+    description: "A structured grid of stars arranged in a repeating, geometric pattern.",
+    url: "https://editor.p5js.org/mromslo/full/UY3zLMGiP",
+    image: "/react_assignments1/week12/assignment12/img/star-grid.gif"
+  },
+];
+
+export default projects;
